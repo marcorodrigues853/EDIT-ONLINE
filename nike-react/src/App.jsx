266ecc0996dfbbccdd2eab323073fd9c20f19adb
Marco Rodrigues <marcoaurelio853@gmail.com>
2025-06-25 22:49:03 +0100

    components

--- nike-react/src/App.jsx
+++ nike-react/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Button from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const idade = 18;
+    const nome = 'Francisca';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const uuid = crypto.randomUUID();
+
+    const cor = 'verde';
+    const corClass = cor === 'red' ? 'vermelho' : 'azul';
+
+    const isRequired = false;
+    const typeOfInput = 'number';
+
+    const min = 3;
+    const max = 6;
+
+    return (
+        <>
+            <div id={uuid} className="primeira segunda terceira quarta">
+                <Button label="Ver mais" />
+                <Button label="Voltar" />
+
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <input
+                required={isRequired}
+                type={typeOfInput}
+                min={min}
+                max={max}
+            />
+            <h1>Vite + React</h1>
+
+            <div className="card">
+                <h1>Nome e idade</h1>
+                <span>
+                    Nome {nome} e idade {idade}
+                </span>
+                {/* <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                    <span style={{ background: 'red', color: 'blue' }}></span>
+                </button> */}
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR\
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ nike-react/src/components/Button.jsx
@@ -0,0 +1,14 @@
+function Button(props) {
+    //  js
+    return <button>{props.label}</button>;
+}
+
+export default Button;
+
+// componente novo que se chame Card
+// name | idade | hobbie | profissão
+
+// button  label // ver mais
+{
+    /* <div className="card"></div> */
+}
